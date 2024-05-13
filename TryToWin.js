f5be8e985e5eeff530c183533ccc0275adef8c28
Somay jalan <jalansomay0@gmail.com>
2024-05-13 20:45:54 +0530

    Adding more code

--- TryToWin.js
+++ TryToWin.js
@@ -1,11 +1,21 @@
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 async function typerwriter(speed){
-    document.getElementById("typewriter").style.opacity=1;
-    var element=document.getElementById("typewriter").innerHTML
-    document.getElementById("typewriter").innerHTML=""
-    console.log(element);
+    document.getElementById("rules").style.opacity=1;
+    var element=document.getElementById("rules").innerHTML
+    document.getElementById("rules").innerHTML=""
     for(let i=0;i<element.length;i++){
-        document.getElementById("typewriter").innerHTML=element.substring(0,i+1)+'<cursor aria-hidden="true"></cursor>'
+        document.getElementById("rules").innerHTML=element.substring(0,i+1)+'<cursor aria-hidden="true"></cursor>'
+        var randSpeed=speed+(Math.random()-0.5)*speed
+        // console.log(randSpeed)
+        await sleep(randSpeed);
+        
+    }
+    document.getElementById("rules").innerHTML=element.substring(0,element.length+1)
+    document.getElementById("rulesText").style.opacity=1;
+    var element=document.getElementById("rulesText").innerHTML
+    document.getElementById("rulesText").innerHTML=""
+    for(let i=0;i<element.length;i++){
+        document.getElementById("rulesText").innerHTML=element.substring(0,i+1)+'<cursor aria-hidden="true"></cursor>'
         var randSpeed=speed+(Math.random()-0.5)*speed
         // console.log(randSpeed)
         await sleep(randSpeed);
@@ -13,4 +23,36 @@ async function typerwriter(speed){
     }
 }
 
+function startAnimation(){
+    document.getElementById("TryToWinFont").style.animation="fadeoutAniamtion 1s forwards";
+    
 
+    setTimeout(function(){
+        // document.getElementById("TryToWinFont").remove()
+        document.getElementById("Startgame").style.display="block";
+        document.getElementById("LetComputerStart").style.display="block";
+    },1000)
+    // document.getElementById("LetComputerStart").style.transition="0.4s";
+
+    setTimeout(function(){
+        typerwriter(50)
+    },1000)
+}
+
+function playerStart(){
+    document.getElementById("Startgame").style.animation="fadeoutAniamtion 1s forwards";
+    document.getElementById("LetComputerStart").style.animation="fadeoutAniamtion 1s forwards";
+    setTimeout(function(){
+        document.getElementById("Startgame").remove()
+        document.getElementById("LetComputerStart").remove()        
+        document.getElementById("TryToWinFont").innerHTML="YOUR TURN"
+        document.getElementById("TryToWinFont").style.animation="fadeinAniamtion 1s forwards";
+        document.getElementById("Try to win").style.cursor="pointer";
+    },1000)
+    document.body.addEventListener('click', clicked, true); 
+
+}
+
+function clicked(event){
+    console.log(event)
+}
--- landing.js
+++ landing.js
@@ -3,7 +3,6 @@ async function typerwriter(speed){
     document.getElementById("typewriter").style.opacity=1;
     var element=document.getElementById("typewriter").innerHTML
     document.getElementById("typewriter").innerHTML=""
-    console.log(element);
     for(let i=0;i<element.length;i++){
         document.getElementById("typewriter").innerHTML=element.substring(0,i+1)+'<cursor aria-hidden="true"></cursor>'
         var randSpeed=speed+(Math.random()-0.5)*speed
@@ -11,26 +10,27 @@ async function typerwriter(speed){
         await sleep(randSpeed);
         
     }
+
 }
 
-function transitionToWinPage(href){
+function transitionToWinPage(){
     document.getElementById("typewriter").style.animation="fadeoutAniamtion 0.3s forwards";
     document.getElementById("Try to win").classList.remove("cardHover");
-    document.getElementById("TryToWinFont").innerHTML="";
-    document.getElementById("Try to win").style.width="700px";
-    document.getElementById("Try to win").style.height="825px";
+    document.getElementById("Try to win").style.width="45.57vw";
+    document.getElementById("Try to win").style.height="94.2vh";
     document.getElementById("Try to win").style.top="2%";
     document.getElementById("Try to win").style.left="27.5%";
-    
+    document.getElementById("Try to win").style.position="fixed";
     document.getElementById("middleCircle").style.animation="fadeoutAniamtion 0.25s forwards";
     document.getElementById("leftTopcircle").style.animation="fadeoutAniamtion 0.25s forwards";
     document.getElementById("crossFirst").style.animation="fadeoutAniamtion 0.25s forwards";
     document.getElementById("crossSecond").style.animation="fadeoutAniamtion 0.25s forwards";
-    document.getElementById("TryToWinFont").innerHTML="TRY TO WIN";
-    document.getElementById("TryToWinFont").style.fontSize="75px";
-    document.getElementById("TicTacToe").style.width="600px";
-    document.getElementById("TicTacToe").style.height="600px";
+    document.getElementById("TryToWinFont").style.fontSize="6vh";
+    document.getElementById("TryToWinFont").style.marginTop="3vh";
+
+    document.getElementById("TicTacToe").style.width="39.06vw";
+    document.getElementById("TicTacToe").style.height="39.06vw";
     setTimeout(function(){
         window.location.href="/TryToWin.html"
-    },450)
+    },500)
 }
